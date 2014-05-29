9838f6e20577b1519d6aa40b4a3956841523de49
Клименок Михаил <iammishgan@gmail.com>
2014-05-29 13:43:23 +0700

    Первый коммит rss2email

---
+++ Klimenok.Nsudotnet.Rss2email/RSS2Email/RSS2Email/RSS2Mail.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.ServiceModel.Syndication;
+using System.Threading;
+using System.Xml;
+
+namespace RSS2Email
+{
+	class Program
+	{
+	    private static DateTime _lastDate;
+
+		private static void Main(string[] args)
+		{
+			_lastDate = DateTime.Now;
+            Thread t = new Thread(new ThreadStart(ThreadProc));
+            t.Start();
+		}
+
+        public static void ThreadProc()
+        {
+            while (true)
+            {
+               CheckNewFeeds();
+               Thread.Sleep(1000);
+            }
+        }
+		
+		private static void CheckNewFeeds()
+		{
+		    var xmlReader = XmlReader.Create("http://bash.im/rss");
+			var syndicationFeed = SyndicationFeed.Load(xmlReader);
+			foreach (var item in syndicationFeed.Items.Where(item => item.PublishDate.Date.CompareTo(_lastDate) > 0))
+			{
+				SendEmail(item);
+            }
+			_lastDate = syndicationFeed.Items.ToArray()[0].PublishDate.Date;
+		}
+
+		private static void SendEmail(SyndicationItem item)
+		{
+            MailMessage mail = new MailMessage("you@yourcompany.com", "user@hotmail.com");
+            SmtpClient client = new SmtpClient();
+            client.Port = 25;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Host = "smtp.google.com";
+            mail.Subject = item.Title.Text;
+            mail.Body = item.Summary.Text;
+            client.Send(mail);
+         }
+	}
+}
